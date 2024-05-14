96ebf032aeca5d944b4faa9f6ffbb1c1b041f1a3
Xage1 <kennethkiarie555@gmail.com>
2024-05-14 09:24:56 -0700

    This

---
+++ utils/redis.js
@@ -0,0 +1,55 @@
+const redis = require('redis');
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+
+		this.client.on('error', (err) => {
+			console.error('Redis client error:', err);
+		});
+	}
+
+	isAlive() {
+		return this.client.connected;
+	}
+
+	async get(key) {
+		return new Promise((resolve, reject) => {
+			this.client.get(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
+	}
+
+	async set(key, value, durationInSeconds) {
+		return new Promise((resolve, reject) => {
+			this.client.set(key, value, 'EX', durationInSeconds, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
+	}
+
+	async del(key) {
+		return new Promise((resolve, reject) => {
+			this.client.del(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
+	}
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
